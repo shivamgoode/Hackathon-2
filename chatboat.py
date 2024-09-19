e6a944327ec9e8242324b401adcb8f0e43517330
shivamgoode <shivam68338@gmail.com>
2024-09-19 13:27:58 +0530

    Add files via upload

---
+++ chatboat.py
@@ -0,0 +1,59 @@
+import numpy as np
+import json
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+words = open('words.pkl', 'rb')
+classes = open('classes.pkl', 'rb')
+model = load_model('chatbot_model.h5')
+
+# Preprocess the input
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# Convert a sentence into a bag of words
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+
+# Get response from the model
+def predict_class(sentence, model):
+    bow_data = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([bow_data]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+# Chat with the chatbot
+print("DairyBot is running!")
+
+while True:
+    message = input("You: ")
+    ints = predict_class(message, model)
+    res = get_response(ints, intents)
+    print("DairyBot:", res)
---
+++ training.py
@@ -0,0 +1,81 @@
+import json
+import numpy as np
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# Load the intents file
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+# Process each intent
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # Tokenize each word in the sentence
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        # Add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize, lower each word, and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+# Create the training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    # Initialize our bag of words
+    bag = []
+    word_patterns = doc[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for w in words:
+        bag.append(1) if w in word_patterns else bag.append(0)
+
+    # Output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# Shuffle our features and convert them into np.array
+random.shuffle(training)
+training = np.array(training)
+
+# Create train and test lists
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+# Build the model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("Model training completed!")
